7dcb1edebca60099a61a41e9fec0b92bfecc162a
Enrica Fedeli-Jaques <enricafedeli@gmail.com>
2020-07-25 13:18:06 +0200

    changed root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :cocktails, only: [:index, :new, :show, :create] do
+  root to: 'cocktails#index'
+  resources :cocktails, only: [:root, :new, :show, :create] do
     resources :doses, only: [:new, :create]
     end
     resources :doses, only: [:destroy]
